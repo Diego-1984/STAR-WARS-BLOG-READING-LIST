ab4788bf47ebcbd8e676c8df207f4dbf0c07b2b6
Diego-1984 <diegomenoruceta@gmail.com>
2023-03-21 10:15:29 +0000

    Hard code terminado

--- src/js/component/characters.jsx
+++ src/js/component/characters.jsx
@@ -9,7 +9,7 @@ const Character=(props)=>{
             <div className="card-body">
                 <h5 className="card-title d-flex justify-content-center">{props.title}</h5>
                 <p className="card-text d-flex text-center">{props.text}</p>
-                <Link to="/descriptionGeonosis" className="btn btn-primary mx-4">Learn more</Link>
+                <Link to= {props.link}className="btn btn-primary mx-4">Learn more</Link>
                 <button type="button" class="btn btn-outline-danger"><i class="fa-solid fa-heart"></i></button>
             </div>
     </div>
@@ -24,25 +24,30 @@ const Characters=()=>{
             title="R2-D2" 
             text="No hay nadie que ame el dolor mismo, que lo busque, lo encuentre y lo quiera, 
             simplemente porque es el dolor."
-            buttom="Learm more"/>
+            buttom="Learm more"
+            link="r2d2"/>
 
         <Character 
             title="C3PO" 
             text="No hay nadie que ame el dolor mismo, que lo busque, lo encuentre y lo quiera, 
             simplemente porque es el dolor."
-            buttom="Learn more"/>
+            buttom="Learn more"
+            link="c3po"/>
+            
 
         <Character 
             title="LUCK"
             text="No hay nadie que ame el dolor mismo, que lo busque, lo encuentre y lo quiera, 
             simplemente porque es el dolor."
-            buttom="Learn more"/>
+            buttom="Learn more"
+            link="/luck"/>
 
         <Character 
             title="LEIA" 
             text="No hay nadie que ame el dolor mismo, que lo busque, lo encuentre y lo quiera, 
             simplemente porque es el dolor."
-            buttom="Learn more"/>
+            buttom="Learn more"
+            link="/leia"/>
         
         </>
     )
--- src/js/layout.js
+++ src/js/layout.js
@@ -1,16 +1,21 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
-
+import { Navbar } from "./component/navbar";
 import { Home } from "./views/home";
 import { Geonosis } from "./views/geonosis";
 import { Cantonica } from "./views/cantonica";
 import { Anoat } from "./views/anoat";
 import { Crait } from "./views/crait";
+import { R2d2 } from "./views/r2d2";
+import { C3po } from "./views/c3po";
 import { Single } from "./views/single";
 import injectContext from "./store/appContext";
+import { Luck } from "./views/luck";
+import { Leia } from "./views/leia";
 
-import { Navbar } from "./component/navbar";
+
+
 
 
 //create your first component
@@ -30,6 +35,10 @@ const Layout = () => {
 						<Route path="/cantonica" element={<Cantonica />} />
 						<Route path="/anoat" element={<Anoat />} />
 						<Route path="/crait" element={<Crait />} />
+						<Route path="/r2d2" element={<R2d2 />} />
+						<Route path="/c3po" element={<C3po />} />
+						<Route path="/luck" element={<Luck />} />
+						<Route path="/leia" element={<Leia />} />
 						<Route path="/single/:theid" element={<Single />} />
 						<Route path="*" element={<h1>Not found!</h1>} />
 					</Routes>
