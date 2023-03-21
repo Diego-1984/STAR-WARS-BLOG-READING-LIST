af3d1e527eb8c5db8d180aa8d7054a7969430050
Diego-1984 <diegomenoruceta@gmail.com>
2023-03-21 10:36:49 +0000

    Hard code terminado

--- src/js/layout.js
+++ src/js/layout.js
@@ -9,19 +9,13 @@ import { Anoat } from "./views/anoat";
 import { Crait } from "./views/crait";
 import { R2d2 } from "./views/r2d2";
 import { C3po } from "./views/c3po";
-import { Single } from "./views/single";
-import injectContext from "./store/appContext";
 import { Luck } from "./views/luck";
 import { Leia } from "./views/leia";
+import injectContext from "./store/appContext";
 
 
-
-
-
-//create your first component
 const Layout = () => {
-	//the basename is used when your project is published in a subdirectory and not in the root of the domain
-	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+	
 	const basename = process.env.BASENAME || "";
 
 	return (
@@ -39,7 +33,6 @@ const Layout = () => {
 						<Route path="/c3po" element={<C3po />} />
 						<Route path="/luck" element={<Luck />} />
 						<Route path="/leia" element={<Leia />} />
-						<Route path="/single/:theid" element={<Single />} />
 						<Route path="*" element={<h1>Not found!</h1>} />
 					</Routes>
 				</ScrollToTop>
