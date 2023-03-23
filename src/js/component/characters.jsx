603a5fa6b2bb03f5c2971e0537a80cd3f1d3c90a
Diego-1984 <diegomenoruceta@gmail.com>
2023-03-23 11:02:05 +0000

    Probando console log en characters

--- src/js/component/characters.jsx
+++ src/js/component/characters.jsx
@@ -10,10 +10,10 @@ function Character(props) {
         <>
             <div className="card mx-1 rounded my-3">
                 <img className="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/1200px-Star_Wars_Logo.svg.png" alt="Card image cap" />
-                <div className="card-body">
+                <div className="card-body bg-black text-white">
                     <h5 className="card-title d-flex justify-content-center">{props.title}</h5>
                     <p className="card-text d-flex text-center">{props.text}</p>
-                    <Link to={props.link} className="btn btn-primary mx-4">Learn more</Link>
+                    <Link to={props.link} className="btn btn-dark mx-4">Learn more</Link>
                     <button onClick={() => heart()} class="btn btn-outline-danger"><i class="fa-solid fa-heart"></i></button>
                 </div>
             </div>
--- src/js/component/dropdown.jsx
+++ src/js/component/dropdown.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const Dropdown=()=>{
     return(
     <>
-    <div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
+    <div class="dropdown ">
+  <button class="btn btn-dark dropdown-toggle" type="button" data-bs-toggle="dropdown">
     Favorites
   </button>
-  <ul class="dropdown-menu">
-    <li><a class="dropdown-item">Action</a></li>
+  <ul class="dropdown-menu bg-dark ">
+    <li><a class="dropdown-item text-white">Action</a></li>
     
   </ul>
 </div>
